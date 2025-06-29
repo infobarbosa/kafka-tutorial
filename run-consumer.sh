78ed2ccb918d99f45287ab28f4ac49f7f5688a1b
Barbosa <infobarbosa@yahoo.com.br>
2025-06-29 18:22:05 -0300

    refactor: Remove ativação do ambiente virtual e simplifica execução do consumidor Python no run-consumer.sh

--- run-consumer.sh
+++ run-consumer.sh
@@ -2,12 +2,10 @@
 
 echo "Iniciando o Consumidor Python..."
 
-# Ativa o ambiente virtual
-source venv/bin/activate
+docker run --rm \
+    --name kafka-consumer-python \
+    --network kafka-tutorial_kafka-net \
+    -e BOOTSTRAP_SERVERS_CONFIG="kafka-kraft-1:9092,kafka-kraft-2:9192,kafka-kraft-3:9292" \
+    -e TOPIC_NAME=tutorial-python \
+    infobarbosa/kafka-consumer-python:latest
 
-# Navega até o diretório do consumidor Python e executa o script
-cd kafka-consumer-python/
-python consumer.py
-
-# Desativa o ambiente virtual (opcional, mas boa prática)
-deactivate
