c9935a4a5ca324258504415de48287e3781c36ff
Barbosa <infobarbosa@yahoo.com.br>
2025-06-29 20:08:15 -0300

    refactor: Corrige nome da variável de tópico para 'KAFKA_TOPIC' no script do produtor

--- run-producer.sh
+++ run-producer.sh
@@ -7,7 +7,7 @@ docker run --rm \
     --name kafka-producer-python \
     --network kafka-tutorial_kafka-net \
     -e BOOTSTRAP_SERVERS_CONFIG="kafka-kraft-1:9092,kafka-kraft-2:9192,kafka-kraft-3:9292" \
-    -e TOPIC_NAME=kafka-tutorial \
+    -e KAFKA_TOPIC=kafka-tutorial \
     infobarbosa/kafka-producer-python:latest
 
 echo "Produtor Python finalizado."
