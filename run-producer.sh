e85884df0e9eba517bd8952d573603ed3f87de63
Barbosa <infobarbosa@yahoo.com.br>
2025-06-29 18:22:30 -0300

    refactor: Atualiza rede no run-producer.sh para usar kafka-tutorial_kafka-net

--- run-producer.sh
+++ run-producer.sh
@@ -5,7 +5,7 @@ echo "Iniciando o Produtor Python..."
 # Executa a imagem infobarbosa/kafka-producer-python:latest
 docker run --rm \
     --name kafka-producer-python \
-    --network kafka-net \
+    --network kafka-tutorial_kafka-net \
     -e BOOTSTRAP_SERVERS_CONFIG="kafka-kraft-1:9092,kafka-kraft-2:9192,kafka-kraft-3:9292" \
     -e TOPIC_NAME=tutorial-python \
     infobarbosa/kafka-producer-python:latest
