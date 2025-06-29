5e9a9f06c4b83eeb0f1a21d59cca99a0bc30786b
Barbosa <infobarbosa@yahoo.com.br>
2025-06-29 19:45:17 -0300

    refactor: Adiciona remoção de imagens antigas antes da construção das imagens Docker para Produtor e Consumidor Python

--- build-images.sh
+++ build-images.sh
@@ -3,6 +3,12 @@
 echo "Iniciando a construção das imagens Docker para clientes Python..."
 
 # 1. Construir a imagem Docker para o Produtor Python
+echo "Removendo imagens antigas do Produtor Python..."
+docker rmi infobarbosa/kafka-producer-python:latest --force 2>/dev/null
+if [ $? -ne 0 ]; then
+    echo "Nenhuma imagem antiga do Produtor Python encontrada ou erro ao removê-la."
+fi
+
 echo "Construindo imagem Docker para o Produtor Python..."
 docker build -t infobarbosa/kafka-producer-python:latest -f ./kafka-producer-python/Dockerfile .
 
@@ -13,6 +19,12 @@ fi
 echo "Imagem do Produtor Python construída com sucesso."
 
 # 2. Construir a imagem Docker para o Consumidor Python
+echo "Removendo imagens antigas do Consumidor Python..."
+docker rmi infobarbosa/kafka-consumer-python:latest --force 2>/dev/null
+if [ $? -ne 0 ]; then
+    echo "Nenhuma imagem antiga do Consumidor Python encontrada ou erro ao removê-la."
+fi
+
 echo "Construindo imagem Docker para o Consumidor Python..."
 docker build -t infobarbosa/kafka-consumer-python:latest -f ./kafka-consumer-python/Dockerfile .
 
