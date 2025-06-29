94ba76a8abe012398e6c3a52422e29b9020caf0e
Barbosa <infobarbosa@yahoo.com.br>
2025-06-29 20:03:49 -0300

    refactor: Atualiza nome do contêiner do consumidor para usar variável e corrige nome da variável de tópico

--- run-consumer.sh
+++ run-consumer.sh
@@ -2,10 +2,12 @@
 
 echo "Iniciando o Consumidor Python..."
 
+# Configura a variavel CONTAINER_NAME para kafka-consumer-python concatenado com um numero aleatório
+CONTAINER_NAME="kafka-consumer-python-$((RANDOM % 9000 + 1000))"
+
 docker run --rm \
-    --name kafka-consumer-python \
+    --name $CONTAINER_NAME \
     --network kafka-tutorial_kafka-net \
     -e BOOTSTRAP_SERVERS_CONFIG="kafka-kraft-1:9092,kafka-kraft-2:9192,kafka-kraft-3:9292" \
-    -e TOPIC_NAME=kafka-tutorial \
+    -e KAFKA_TOPIC=kafka-tutorial \
     infobarbosa/kafka-consumer-python:latest
-
