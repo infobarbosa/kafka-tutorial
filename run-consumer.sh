8a6541bc7d3a67ef3e24158ccf5968368532ee3a
Barbosa <infobarbosa@yahoo.com.br>
2025-06-29 19:45:24 -0300

    refactor: Atualiza nome do tópico para 'kafka-tutorial' nos scripts do produtor e consumidor

--- run-consumer.sh
+++ run-consumer.sh
@@ -6,6 +6,6 @@ docker run --rm \
     --name kafka-consumer-python \
     --network kafka-tutorial_kafka-net \
     -e BOOTSTRAP_SERVERS_CONFIG="kafka-kraft-1:9092,kafka-kraft-2:9192,kafka-kraft-3:9292" \
-    -e TOPIC_NAME=tutorial-python \
+    -e TOPIC_NAME=kafka-tutorial \
     infobarbosa/kafka-consumer-python:latest
 
--- run-producer.sh
+++ run-producer.sh
@@ -7,7 +7,7 @@ docker run --rm \
     --name kafka-producer-python \
     --network kafka-tutorial_kafka-net \
     -e BOOTSTRAP_SERVERS_CONFIG="kafka-kraft-1:9092,kafka-kraft-2:9192,kafka-kraft-3:9292" \
-    -e TOPIC_NAME=tutorial-python \
+    -e TOPIC_NAME=kafka-tutorial \
     infobarbosa/kafka-producer-python:latest
 
 echo "Produtor Python finalizado."
